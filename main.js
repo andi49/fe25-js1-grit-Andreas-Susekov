4b63fcb8748965a8392abf3d13a15782f56faf15
andi49 <46962871+andi49@users.noreply.github.com>
2025-11-05 17:03:11 +0100

    Now you can see the temp in city your search by

--- main.js
+++ main.js
@@ -1,24 +1,32 @@
 const fetchAPI = async() => {
     try {
+        const userInput = document.getElementById('cityInput').value.trim()
+
         const URL = `https://api.openweathermap.org/data/2.5/forecast?q=${userInput}&units=metric&appid=5bcf228078a03cf5a40ce183c62881ad`
-         const response = await fetch(URL)
-         const data = await response.json()
-         console.log(data.city.name)
+        const response = await fetch(URL)
+        const data = await response.json()
+        console.log(data.city.name)
         console.log(data.city)
         // console.log(data.list[0].main.feels_like)
         console.log(data)
 
-        document.getElementById('searchbtn').addEventListener('click', () => {
-            const userInput = document.getElementById('cityInput').value.trim()
-           const result = document.getElementById('capitalName')
 
-            
-           result.innerHTML = `${data.city.name}`
-           
-        })  
+
+        return data;
     } catch (error) {
          console.log(error);
     }
 }
-fetchAPI()
 
+document.getElementById('searchbtn').addEventListener('click', async () => {
+    const cityData = await fetchAPI()
+
+    const cityName = cityData.city.name
+    document.getElementById('capitalName').innerHTML = cityName;
+
+    const getTemp = cityData.list[0].main.temp
+     document.getElementById('temp').innerHTML = getTemp + ' ' + 'grader'
+    
+    // const result = document.querySelector('.temp')
+    // result.innerHTML = cityData.list[0].main.temp
+})
