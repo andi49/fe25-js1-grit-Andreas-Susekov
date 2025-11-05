f177ff9908f4411bddbc6669c6b0816245121a7b
andi49 <46962871+andi49@users.noreply.github.com>
2025-11-05 20:11:31 +0100

    Now you change if you want in Celsius or Fahrenheit

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
-const fetchAPI = async() => {
-    try {
-        const userInput = document.getElementById('cityInput').value.trim()
-        const iconTemp = document.getElementById('weatherIcon')
+const fetchAPI = async(unit) => {
 
-        const URL = `https://api.openweathermap.org/data/2.5/forecast?q=${userInput}&units=metric&appid=5bcf228078a03cf5a40ce183c62881ad`
-      
-        const response = await fetch(URL)
+    try {
         
+        const userInput = document.getElementById('cityInput').value.trim()
+        const URL = `https://api.openweathermap.org/data/2.5/forecast?q=${userInput}&units=${unit}&appid=5bcf228078a03cf5a40ce183c62881ad`
+        const response = await fetch(URL)
         const data = await response.json()
         console.log(data.city.name)
         console.log(data.city)
         // console.log(data.list[0].main.feels_like)
         console.log(data)
-
-
 
         return data;
     } catch (error) {
@@ -23,22 +19,22 @@ const fetchAPI = async() => {
     
 
     document.getElementById('searchbtn').addEventListener('click', async () => {
-    const cityData = await fetchAPI()
 
-   
+
+    const getTempK = document.getElementById('unitToggle').checked
+    const unit = getTempK ? 'imperial' : 'metric'
+    const unitSymbol = getTempK ? '°F' : '°C'
+
+    const cityData = await fetchAPI(unit)
 
     const cityName = cityData.city.name
     document.getElementById('capitalName').innerHTML = cityName;
 
     const getTemp = cityData.list[0].main.temp
-    document.getElementById('temp').innerHTML = getTemp + ' ' + 'celcius'
+    document.getElementById('temp').innerHTML = `${getTemp} ${unitSymbol}`;
+  
 
-    if (Math.round(getTemp) == 10) {
-       const bkColor = document.querySelector('.boxtwo')
-       bkColor.style.backgroundColor = 'blue'
-    }
-
-     const getWindSpeed = cityData.list[0].wind.speed
+ const getWindSpeed = cityData.list[0].wind.speed
     document.getElementById('windspeed').innerHTML = getWindSpeed + ' ' + 'm/s'
     
 
@@ -48,6 +44,9 @@ const fetchAPI = async() => {
 
     iconTemp.src = URLICON
 
-   
+     if (Math.round(getTemp) == 10) {
+       const bkColor = document.querySelector('.boxtwo')
+       bkColor.style.backgroundColor = 'blue'
+    }
 
 })
