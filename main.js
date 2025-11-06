3de67777d0fd621099ca41cb8431c0acafc52612
andi49 <46962871+andi49@users.noreply.github.com>
2025-11-06 16:41:37 +0100

    Removed unit toggle

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const fetchAPI = async(unit) => {
+const fetchAPI = async() => {
 
     try {
         
         const userInput = document.getElementById('cityInput').value.trim()
-        const URL = `https://api.openweathermap.org/data/2.5/forecast?q=${userInput}&units=${unit}&appid=5bcf228078a03cf5a40ce183c62881ad`
+        const URL = `https://api.openweathermap.org/data/2.5/forecast?q=${userInput}&units=metric&appid=5bcf228078a03cf5a40ce183c62881ad`
         const response = await fetch(URL)
         const data = await response.json()
         console.log(data.city.name)
@@ -20,31 +20,25 @@ const fetchAPI = async(unit) => {
 
     document.getElementById('searchbtn').addEventListener('click', async () => {
 
-
-    const getTempK = document.getElementById('unitToggle').checked
-    const unit = getTempK ? 'imperial' : 'metric'
-    const unitSymbol = getTempK ? '°F' : '°C'
-    const windUnit = getTempK ? 'mph' : 'm/s';
-
-    const cityData = await fetchAPI(unit)
+    const cityData = await fetchAPI()
 
     const cityName = cityData.city.name
     document.getElementById('capitalName').innerHTML = cityName;
 
-    const getTemp = cityData.list[0].main.temp
-    document.getElementById('temp').innerHTML = `${Math.round(getTemp)} ${unitSymbol}`;
-        console.log()
+    const getTemp = Math.round(cityData.list[0].main.temp)
+    document.getElementById('temp').innerHTML = `${getTemp} °C`;
+       
          const bkColor = document.querySelector('.boxtwo')
-        if (`${getTemp}` === 11) {
+        if (`${getTemp}` == 13) {
        bkColor.style.backgroundColor = 'hsl(40, 100%, 50%)'
-        } else if (`${getTemp}` >= 15){  
+        } else if (`${getTemp}` == 15){  
             bkColor.style.backgroundColor = 'hsl(30, 100%, 50%)'
         } else {
 
         }
 
  const getWindSpeed = cityData.list[0].wind.speed
-    document.getElementById('windspeed').innerHTML = `${getWindSpeed} ${windUnit}`
+    document.getElementById('windspeed').innerHTML = `${getWindSpeed}m/s`
     
 
     const iconTemp = document.getElementById('weatherIcon')
@@ -53,9 +47,36 @@ const fetchAPI = async(unit) => {
 
     iconTemp.src = URLICON
 
-     if (Math.round(getTemp) == 10) {
-       const bkColor = document.querySelector('.boxtwo')
-       bkColor.style.backgroundColor = ''
-    }
+    
+     for (let i = 1; i <= 5; i++) {
+    const realDiv = document.getElementById('container')
+    realDiv.classList.add('container')
+    const fakeTemp = document.createElement('div')
+    const fakeh1 = document.createElement('h1')
+    const fakeh2 = document.createElement('h2')
+    const fakeP = document.createElement('ps')
 
+    realDiv.appendChild(fakeTemp)
+    fakeTemp.appendChild(fakeh1)
+    fakeTemp.appendChild(fakeh2)
+    fakeTemp.appendChild(fakeP)
+ 
+        
+
+    fakeh1.innerHTML = Math.round(cityData.list[i].main.temp) + '°C'
+    fakeh2.innerHTML = cityData.list[i].wind.speed
+    
+    
+        
+  
+    
+    
+    }
+    
+    
+
+
+
+
+     
 })
